ab5d9775c60b007bd607bdb9c44ac9448a6a282b
Archita Patel <architapatelis@gmail.com>
2017-09-10 20:35:49 -0700

    checkpoint 4 - assignment completed

--- db/seed.rb
+++ db/seed.rb
@@ -4,52 +4,31 @@ require 'bloc_record'
 
 BlocRecord.connect_to('db/address_bloc.sqlite')
 
-# tests for checkpoint 2 - assignment
+#tests:
 
 puts "#1"
-puts Entry.find_by(:name, 'Foo One')
+puts Entry.where("phone_number = '999-999-9999'")
 puts "----------------------------------------------"
 
 puts "#2"
-puts Entry.find_by_name('Foo One')
+puts Entry.where(name: 'Foo Three')
 puts "----------------------------------------------"
 
 puts "#3"
-puts Entry.find_each(start: 2, batch_size: 3)
+puts Entry.order("phone_number")
 puts "----------------------------------------------"
 
 puts "#4"
-puts Entry.find_each(batch_size: 3)
+puts Entry.order(:phone_number)
 puts "----------------------------------------------"
 
 puts "#5"
-puts Entry.find_each(start: 2)
+puts Entry.order("phone_number DESC")
 puts "----------------------------------------------"
 
 puts "#6"
-puts Entry.find_each()
+puts Entry.order(name: :asc)
 puts "----------------------------------------------"
-
-puts "#7"
-puts Entry.find_in_batches(start: 2, batch_size: 3)
-puts "----------------------------------------------"
-
-puts "#8"
-puts Entry.find_in_batches(batch_size: 3)
-puts "----------------------------------------------"
-
-puts "#9"
-puts Entry.find_in_batches(start: 2)
-puts "----------------------------------------------"
-
-puts "#10"
-puts Entry.find_in_batches()
-
-puts "#11"
-puts Entry.find_by_age() #=> find_by_age is not a valid method.
-
-
-
 
 
 # book = AddressBook.create(name: 'My Address Book')
