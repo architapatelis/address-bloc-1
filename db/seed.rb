eb323cb1312bb02aa0a224da3b5f88d570710c69
Archita Patel <architapatelis@gmail.com>
2017-09-11 21:13:39 -0700

    checkpoint 5 assignment completed

--- db/seed.rb
+++ db/seed.rb
@@ -7,28 +7,28 @@ BlocRecord.connect_to('db/address_bloc.sqlite')
 #tests:
 
 puts "#1"
-puts Entry.where("phone_number = '999-999-9999'")
-puts "----------------------------------------------"
+people = { 3 => { "name" => "John Oliver" }, 4 => { "email" => "matt@gmail.com" } }
+Entry.update(people.keys, people.values)
+puts "------------------------------------------------------------"
+
 
 puts "#2"
-puts Entry.where(name: 'Foo Three')
-puts "----------------------------------------------"
+e = Entry.all[4]
+puts e.update_name("Kate Smith")
+puts e.update_email("kate@gmail.com")
+puts "------------------------------------------------------------"
 
 puts "#3"
-puts Entry.order("phone_number")
-puts "----------------------------------------------"
+puts Entry.where(name: 'Kate Smith').take
+puts "------------------------------------------------------------"
 
 puts "#4"
-puts Entry.order(:phone_number)
-puts "----------------------------------------------"
+puts Entry.where(name: 'Kate Smith').where(email: 'kate@gmail.com')
+puts "------------------------------------------------------------"
 
 puts "#5"
-puts Entry.order("phone_number DESC")
-puts "----------------------------------------------"
-
-puts "#6"
-puts Entry.order(name: :asc)
-puts "----------------------------------------------"
+puts Entry.where(name: 'Kate Smith').not(email: 'kate@gmail.com')
+puts "------------------------------------------------------------"
 
 
 # book = AddressBook.create(name: 'My Address Book')
